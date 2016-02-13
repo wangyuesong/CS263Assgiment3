497b13a632bb8fa55c304cc7d8f7c273e1a86e18
wangyuesong <573978301@qq.com>
2016-02-12 16:35:02 -0800

    Professor: https://github.com/wangyuesong/WYSBuilder-Frontend https://github.com/wangyuesong/WYSBuilder-Backend These are my new repos. Thanks!

--- base/src/main/java/login/SignInServlet.java
+++ base/src/main/java/login/SignInServlet.java
@@ -1,39 +1,39 @@
-package login;
-
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.auth.RequestToken;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import java.io.IOException;
-
-@SuppressWarnings("serial")
-public class SignInServlet extends HttpServlet {
-    
-    /**
-     * Method doGet
-     * 
-     * Creates an signin url for twitter.
-     */
-    
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        Twitter twitter = new TwitterFactory().getInstance();
-        req.getSession().setAttribute("twitter", twitter);
-        try {
-            StringBuffer callbackURL = req.getRequestURL();
-            int index = callbackURL.lastIndexOf("/");
-            callbackURL.replace(index, callbackURL.length(), "").append("/callback");
-
-            RequestToken requestToken = twitter.getOAuthRequestToken(callbackURL.toString());
-            req.getSession().setAttribute("requestToken", requestToken);
-            res.sendRedirect(requestToken.getAuthenticationURL());
-        } catch (TwitterException e) {
-            throw new ServletException(e);
-        }
-    }
-}
+//package login;
+//
+//import twitter4j.Twitter;
+//import twitter4j.TwitterException;
+//import twitter4j.TwitterFactory;
+//import twitter4j.auth.RequestToken;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import java.io.IOException;
+//
+//@SuppressWarnings("serial")
+//public class SignInServlet extends HttpServlet {
+//    
+//    /**
+//     * Method doGet
+//     * 
+//     * Creates an signin url for twitter.
+//     */
+//    
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+//        Twitter twitter = new TwitterFactory().getInstance();
+//        req.getSession().setAttribute("twitter", twitter);
+//        try {
+//            StringBuffer callbackURL = req.getRequestURL();
+//            int index = callbackURL.lastIndexOf("/");
+//            callbackURL.replace(index, callbackURL.length(), "").append("/callback");
+//
+//            RequestToken requestToken = twitter.getOAuthRequestToken(callbackURL.toString());
+//            req.getSession().setAttribute("requestToken", requestToken);
+//            res.sendRedirect(requestToken.getAuthenticationURL());
+//        } catch (TwitterException e) {
+//            throw new ServletException(e);
+//        }
+//    }
+//}
